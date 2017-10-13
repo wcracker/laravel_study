7af6522012bbc2cdc19e2f4d18ec460780f84257
joyfun <woody_2006@163.com>
2017-10-13 09:04:33 +0000

    use sub layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Laravel') - woody</title>
-</head>
-<body>
-    @yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Woody</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+    </div>
+    @include('layouts._footer')
+  </body>
 </html>
