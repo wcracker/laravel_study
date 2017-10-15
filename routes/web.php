a43387e83fa0f9a05a4fb5de1bff705289e6764b
joyfun <woody_2006@163.com>
2017-10-15 21:55:15 +0800

    Finish Login and Logout

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UserController@create')->name('signup');
-
 Route::resource('users', 'UserController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('login', 'SessionsController@destroy')->name('logout');
